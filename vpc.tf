f83de44cc30960d4cd5887b87bdbe9c5b3817ca9
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-09-18 21:03:21 +0530

    adding the vpc

--- vpc.tf
+++ vpc.tf
@@ -1,92 +1,93 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
 
-  tags = {
-    Name = "vpc"
-  }
-}
+#   tags = {
+#     Name = "vpc"
+#   }
+# }
 
-# Create subnet
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+# # Create subnet
+# resource "aws_subnet" "public" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.1.0/24"
 
-  tags = {
-    Name = "public_subnets"
-  }
-}
+#   tags = {
+#     Name = "public_subnets"
+#   }
+# }
 
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+# resource "aws_subnet" "private" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.2.0/24"
 
-  tags = {
-    Name = "private_subnets"
-  }
-}
+#   tags = {
+#     Name = "private_subnets"
+#   }
+# }
 
-# internet_gateway
+# # internet_gateway
 
-resource "aws_internet_gateway" "automated-igw" {
-  vpc_id = aws_vpc.main.id
+# resource "aws_internet_gateway" "automated-igw" {
+#   vpc_id = aws_vpc.main.id
 
-  tags = {
-    Name = "automated-igw"
-  }
-}
+#   tags = {
+#     Name = "automated-igw"
+#   }
+# }
 
-# Nat Gateway
+# # Nat Gateway
 
-resource "aws_nat_gateway" "nat_gateway" {
-  subnet_id     = aws_subnet.public.id
+# resource "aws_nat_gateway" "nat_gateway" {
+#   subnet_id     = aws_subnet.public.id
 
-  tags = {
-    Name = "gw NAT"
-  }
+#   tags = {
+#     Name = "gw NAT"
+#   }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.automated-igw]
-}
+#   # To ensure proper ordering, it is recommended to add an explicit dependency
+#   # on the Internet Gateway for the VPC.
+#   depends_on = [aws_internet_gateway.automated-igw]
+# }
 
-# Route Tables
+# # Route Tables
 
-resource "aws_route_table" "public-rt" {
-  vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "10.0.1.0/24"
-    gateway_id = aws_internet_gateway.automated-igw.id
-  }
+# resource "aws_route_table" "public-rt" {
+#   vpc_id = aws_vpc.main.id
+
+#   route {
+#     cidr_block = "10.0.1.0/24"
+#     gateway_id = aws_internet_gateway.automated-igw.id
+#   }
 
   
 
-  tags = {
-    Name = "public-rt"
-  }
-}
+#   tags = {
+#     Name = "public-rt"
+#   }
+# }
 
-resource "aws_route_table" "private-rt" { 
-    vpc_id = aws_vpc.main.id
+# resource "aws_route_table" "private-rt" { 
+#     vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gateway.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.nat_gateway.id
+#   }
 
-   tags = {
-    Name = "private-route-table"
-  } 
-}
+#    tags = {
+#     Name = "private-route-table"
+#   } 
+# }
 
 
-#route table association terraform
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public-rt.id
-}
+# #route table association terraform
+# resource "aws_route_table_association" "public" {
+#   subnet_id      = aws_subnet.public.id
+#   route_table_id = aws_route_table.public-rt.id
+# }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private-rt.id
-}
+# resource "aws_route_table_association" "private" {
+#   subnet_id      = aws_subnet.private.id
+#   route_table_id = aws_route_table.private-rt.id
+# }
