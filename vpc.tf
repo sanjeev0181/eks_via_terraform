96a24ac0d23bd4d1f355e69b0777c9d5c8b07121
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-09-18 20:10:34 +0530

    adding the vpc

---
+++ vpc.tf
@@ -0,0 +1,79 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "vpc"
+  }
+}
+
+# Create subnet
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public_subnets"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private_subnets"
+  }
+}
+
+# internet_gateway
+
+resource "aws_internet_gateway" "automated-igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "automated-igw"
+  }
+}
+
+
+# Route Tables
+
+resource "aws_route_table" "public-rt" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.automated-igw.id
+  }
+
+  
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table" "private-rt" { 
+    vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.example.id
+  }
+
+   tags = {
+    Name = "private-route-table"
+  } 
+}
+
+
+#route table association terraform
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnets_cidr.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private_subnets_cidr.id
+  route_table_id = aws_route_table.private-route-table.id
+}
