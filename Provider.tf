5fd17d26a1cb0596dbf8826b2919000b8ee418ad
sanjeev01811 <adityasanjeev1996@gmail.com>
2023-10-05 16:34:25 +0530

    adding

--- Provider.tf
+++ Provider.tf
@@ -14,10 +14,21 @@ provider "aws" {
 }
 # Create S3 Bucket
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-jenkins-bucket02"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-jenkins-bucket02"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
+
+resource "aws_s3_object" "object" {
+  bucket = "terraform_suppu_s3_bucket"
+  key    = "terraform.tfstate"
+  source = "path/to/file"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  # etag = filemd5("path/to/file")
 }
